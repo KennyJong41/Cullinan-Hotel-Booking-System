a6ba1d680e54f65876b4bfc4dca7836e07b30702
KennyJong41 <kennyjong10@gmail.com>
2025-12-04 09:40:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.neon.tech',
-      },
-    ],
-  },
+  reactStrictMode: true,
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
